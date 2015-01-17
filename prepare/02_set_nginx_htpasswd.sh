401aad254ec3f70c67db121cd503e12fdcef0346
Stephen Ausman <stephenausman@gmail.com>
2015-01-17 17:40:42 -0600

    Add missing space

--- prepare/02_set_nginx_htpasswd.sh
+++ prepare/02_set_nginx_htpasswd.sh
@@ -6,6 +6,6 @@ if [ ! -z "$NGINX_AUTH_BASIC" ]; then
   echo $NGINX_AUTH_BASIC |tr , '\n' > /etc/nginx/.htpasswd
 
   # update nginx config
-  sed -i "s/#_auth_basic_/auth_basic \"Restricted\"; auth_basic_user_file \/etc\/nginx\/.htpasswd;/" \
+  sed -i "s/# _auth_basic_/auth_basic \"Restricted\"; auth_basic_user_file \/etc\/nginx\/.htpasswd;/" \
     /etc/nginx/sites-available/default
 fi
